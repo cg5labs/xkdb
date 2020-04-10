5a404977ce6ad0cf57472de815559f18569914e9
Chris Scheible <cgscheible@yahoo.com>
2020-04-10 20:48:59 +0800

    Initial.

---
+++ upload.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import argparse
+import dropbox
+import os
+import pathlib
+import re
+import sys
+import uuid
+
+target_filename = str(uuid.uuid4())[:8] + ".kdbx"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", help="file path to upload")
+args = parser.parse_args()
+
+api_token = os.environ['API_TOKEN']
+
+# the source file
+local_filename = pathlib.Path(args.file)    # located in this folder
+if local_filename.exists():
+  filepath = local_filename  # path object, defining the file
+
+# target location in Dropbox
+target = "/"              # the target folder
+#targetfile = target + target_filename + ".txt"   # the target path and file name
+targetfile = target + target_filename  # the target path and file name
+
+# Create a dropbox object using an API v2 key
+d = dropbox.Dropbox(api_token)
+
+# open the file and upload it
+with filepath.open("rb") as f:
+   # upload gives you metadata about the file
+   # we want to overwite any previous version of the file
+   meta = d.files_upload(f.read(), targetfile, mode=dropbox.files.WriteMode("overwrite"))
+
+# create a shared link
+link = d.sharing_create_shared_link(targetfile)
+
+# url which can be shared
+url = link.url
+
+# link which directly downloads by replacing ?dl=0 with ?dl=1
+dl_url = re.sub(r"\?dl\=0", "?dl=1", url)
+print (dl_url)
---
+++ upload.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+UPLOAD=$1
+RC=0
+APP_PATH=/Users/User/coding_local/python/xkdb
+
+cd ${APP_PATH}
+
+if [[ -z ${UPLOAD} ]]; then
+  echo "Usage: $(basename $0) <filename>"
+  RC=1
+fi
+
+if [[ ! -f ${UPLOAD} ]]; then
+  echo "Upload file not found! ${UPLOAD}"
+  RC=2
+fi
+
+if [[ $RC == 0 ]]; then
+  if [[ -d venv/bin ]]; then
+    source venv/bin/activate
+    source $HOME/.dropbox
+    ./upload.py -f /Users/User/Documents/Firefox.kdbx
+    RC=$?
+  else
+    echo "VirtualEnv missing. Instaling Virtualenv ... "
+    virtualenv -p /usr/local/bin/python3 venv
+    source venv/bin/activate
+    pip3 install -r requirements.txt
+    RC=3
+  fi
+fi
+
+exit $RC
