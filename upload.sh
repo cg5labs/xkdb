f42171ede5efcd2fdcd6c859b97a402c62264f96
Chris Scheible <cgscheible@yahoo.com>
2020-05-11 13:07:35 +0800

    upload.sh - refactor

--- upload.sh
+++ upload.sh
@@ -10,24 +10,24 @@ RC=0
 
 cd ${APP_PATH} || exit 1
 
+# sanity checks
+
+# check input parameter
 if [[ -z ${UPLOAD} ]]; then
   echo "Usage: $(basename "$0") <filename>"
   RC=1
 fi
 
+# check if local file exists
 if [[ ! -f ${UPLOAD} ]]; then
-  echo "Upload file not found! ${UPLOAD}"
+  echo "Upload file not found: ${UPLOAD}"
   RC=2
 fi
 
-if [[ $RC == 0 ]]; then
-  if [[ -d venv/bin ]]; then
-    # shellcheck source=/dev/null
-    source venv/bin/activate
-    # shellcheck source=/dev/null
-    source "${CFG_PATH}"/.dropbox
-    ./upload.py -f "${UPLOAD}"
-    RC=$?
+# check if Python venv exists
+if [[ -d venv/bin ]]; then
+  # shellcheck source=/dev/null
+  source venv/bin/activate
   else
     echo "VirtualEnv missing. Instaling Virtualenv ... "
     virtualenv -p /usr/local/bin/python3 venv
@@ -35,7 +35,21 @@ if [[ $RC == 0 ]]; then
     source venv/bin/activate
     pip3 install -r requirements.txt
     RC=3
-  fi
+fi
+
+# check if config file exists
+if [[ -r "${CFG_PATH}/.dropbox" ]]; then
+  # shellcheck source=/dev/null
+  source "${CFG_PATH}"/.dropbox
+else
+  echo "Config file not found: ${CFG_PATH}/.dropbox"
+  RC=3
+fi
+
+# main
+if [[ $RC == 0 ]]; then
+  ./upload.py -f "${UPLOAD}"
+  RC=$?
 fi
 
 exit $RC
